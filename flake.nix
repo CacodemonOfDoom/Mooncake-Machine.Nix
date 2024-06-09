0e10078cb367f917995162c81f637de57d25291f
CacodemonOfDoom <fabio9000dg@gmail.com>
2024-06-09 20:58:55 +0200

    leaves a symlink named [4mresult[24m in the current directory, which points to the output of  the                leaves   a   symlink   [4mresult[24m   in   the   current   directory    that    points    (under                is  necessary  if  the  NixOS modules use features not provided by the currently installed                fore the “current” generation of the Nix profile [4m/nix/var/nix/profiles/system[24m.)                Instead of using the Nix profile [4m/nix/var/nix/profiles/system[24m to keep track of the current                menu named “NixOS - Profile [4mname[24m” in GRUB’s boot menu, containing the current and previous                output named ‘nixosConfigurations.[4mname[24m’. If [4mname[24m is omitted, it  default  to  the  current                build non-flake NixOS configurations even if the current NixOS systems uses flakes.                Expose the the current environment variables to post activation scripts. Will  skip  usage        [4m/run/current-system[0m                A symlink to the currently active system configuration in the Nix store.                The Nix profile that contains the current and previous system configurations. Used to gen‐

--- flake.nix
+++ flake.nix
@@ -1,19 +1,19 @@
 {
   inputs = {
-	home-manager =  { 
-		url = "github:nix-community/home-manager";
-		inputs.nixpkgs.follows = "nixpkgs";
-   	};
-	nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+	  nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     	chaotic.url = "github:chaotic-cx/nyx/nyxpkgs-unstable";
   	  nixos-hardware.url = "github:NixOS/nixos-hardware";
     	nix-alien.url = "github:thiagokokada/nix-alien";
     	nix-gaming.url = "github:fufexan/nix-gaming";
     	rust-overlay.url = "github:oxalica/rust-overlay";
     	nix-vscode-extensions.url = "github:nix-community/nix-vscode-extensions";
+
+    wrapper-manager = {
+      url = "github:viperML/wrapper-manager";
+    };
   };
 
-  outputs = {home-manager, chaotic, self, nixpkgs, ...} @ inputs: {
+  outputs = {wrapper-manager, chaotic, self, nixpkgs, ...} @ inputs: {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       specialArgs = {inherit inputs;}; # this is the important part
       modules = [
