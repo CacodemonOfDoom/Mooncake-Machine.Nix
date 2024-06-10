1ad8ae5a8597509565ebc1b58a775f7458d25597
CacodemonOfDoom <fabio9000dg@gmail.com>
2024-06-10 14:34:44 +0200

    [1mbuild   [22mBuild  the  new configuration, but neither activate it nor add it to the GRUB boot menu. It leaves a symlink named [4mresult[24m in the current directory, which points to the output of the top-level “system” derivation. This is es‐                Build  a script that starts a NixOS virtual machine with the desired configuration. It leaves a symlink [4mresult[24m in the current directory that points (under ‘result/bin/run-[4mhostname[24m-vm’) at the script that starts the VM. Thus,                tures not provided by the currently installed version of Nix. This option disables building a new Nix.                Instead of building a new configuration as specified by [4m/etc/nixos/configuration.nix[24m, roll back to the previous configuration. (The previous configuration is defined as the one before the “current” generation of the Nix pro‐                Instead  of  using  the  Nix profile [4m/nix/var/nix/profiles/system[24m to keep track of the current and previous system configurations, use [4m/nix/var/nix/profiles/system-profiles/name[24m. When you use GRUB 2, for every system profile                created with this flag, NixOS will create a submenu named “NixOS - Profile [4mname[24m” in GRUB’s boot menu, containing the current and previous configurations of this profile.                [4mname[24m is omitted, it default to the current host name.                Do not imply [1m--flake [22mif [4m/etc/nixos/flake.nix[24m exists. With this option, it is possible to build non-flake NixOS configurations even if the current NixOS systems uses flakes.                Expose the the current environment variables to post activation scripts. Will skip usage of [1msystemd-run [22mduring system activation. Possibly dangerous, specially in remote environments (e.g.: via SSH). Will be removed  in  the        [4m/run/current-system[0m                A symlink to the currently active system configuration in the Nix store.                The Nix profile that contains the current and previous system configurations. Used to generate the GRUB boot menu.

--- configuration.nix
+++ configuration.nix
@@ -101,7 +101,7 @@
   # Enable automatic login for the user.
   services.xserver.displayManager.autoLogin.enable = true;
   services.xserver.displayManager.autoLogin.user = "apricorum";
-
+  services.displayManager.enable = true;
   # Workaround for GNOME autologin: https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229
   systemd.services."getty@tty1".enable = false;
   systemd.services."autovt@tty1".enable = false;
